b200dd6f2a5b3afc89d8b2add6b80d36a9d74f73
Nabeel Valley <nabeelvalley@outlook.com>
2019-05-08 14:24:55 +0200

    :heavy_plus_sign: add NetworkInterfaces cordova

--- mobile/src/app/home/home.page.ts
+++ mobile/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NetworkInterface } from '@ionic-native/network-interface/ngx';
 import * as Mqtt from '../lib/browserMqtt'
 import * as mqtt from 'mqtt'
 
@@ -8,22 +9,26 @@ import * as mqtt from 'mqtt'
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  client: mqtt.MqttClient
+  client: mqtt.MqttClient;
+  wifiIpAddress: string;
 
-  constructor() {
-    this.client = Mqtt.connect('ws://192.168.0.113:1884')
-    this.client.on('connect', () => {
-      console.log('connected to ws')
-    })
-
-    this.client.on('error', (err) => {
-      console.log(err)
+  constructor(private networkInterface: NetworkInterface) {
+    this.networkInterface.getWiFiIPAddress().then((ip) => {
+      this.wifiIpAddress = ip;
     })
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.client.on('connect', () => {
+      console.log('connected to ws');
+    })
+
+    this.client.on('error', (err) => {
+      console.log(err);
+    })
+  }
 
   sendMessage(topic: string, message: string): void {
-    this.client.publish(topic, message)
+    this.client.publish(topic, message);
   }
 }
